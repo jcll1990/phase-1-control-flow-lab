48e3ed247cf1de170ae34d32026ad3e1e45df3d8
Juan Carlos Lopez Larco <Jclopezlarco@gmail.com>
2023-08-21 21:05:40 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+return ride <= 400 ? 'This one is on me!' : ride <= 2000? 'That will be twenty bucks.' : ride <= 2500 ? 'I will gladly take your thirty bucks.' : 'No can do.'
+ 
+/*
+LO MISMO QUE: 
+
+if ( ride <= 400) {
+  return 'This one is on me!'
+}
+else if (ride <= 2000)  {
+  return 'That will be twenty bucks.'
+}
+else if (ride <= 2500)  {
+  return 'I will gladly take your thirty bucks.'
+}
+else {
+ return 'No can do.'
+}
+*/
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  return city === "NYC" ? 'Ok, sounds good.' : "No go."
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case "thanks for everything":
+      return "Bye.";
+    default:
+      return "Thank you!";
+  }
 }
